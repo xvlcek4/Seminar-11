fe898c30e308e9ca45dd827f1034ee408bfb45df
unknown <xvlcek4@ad.fi.muni.cz>
2022-04-28 10:34:05 +0200

    Fixed some errors  Please enter the commit message for your changes. Lines starting

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,6 @@
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+# taken from 
+# https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import cPickle
 import subprocess
 import base64
@@ -13,7 +14,7 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
+def someLongnameForAFunction(request, user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
@@ -32,4 +33,3 @@ def index():
     import_urlib_version(module)
 
 
-print(base64.b64encode(pickle.dumps(RunBinSh())))
